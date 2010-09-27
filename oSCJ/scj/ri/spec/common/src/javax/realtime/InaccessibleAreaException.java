25a5ffb410f34a32afe542b9ca72e0ddf1a9fffd
aplsek <aplsek@gmail.com>
2010-09-27 10:39:09 -0400

    SCJ memory safety - Exceptions:
    ThrowBoundaryError
    --> duplicated in "realtime" and "safety-critical" packages
    --> the realtime one is only infrastructure only while the "safety-critical" is @SCJAllowed!!!??????
    
    
    MemoryAccessError
    - added, was missing in oSCJ

--- oSCJ/scj/ri/spec/common/src/javax/realtime/InaccessibleAreaException.java
+++ oSCJ/scj/ri/spec/common/src/javax/realtime/InaccessibleAreaException.java
@@ -24,6 +24,7 @@ package javax.realtime;
 import java.io.Serializable;
 
 import javax.safetycritical.annotate.SCJAllowed;
+import javax.safetycritical.annotate.SCJRestricted;
 
 /**
  * TBD: do we make this SCJAllowed? It may be that the restrictions put in place
@@ -35,11 +36,29 @@ import javax.safetycritical.annotate.SCJAllowed;
 public class InaccessibleAreaException extends RuntimeException implements
         Serializable {
 
+	/**
+	   * Shall not copy "this" to any instance or
+	   * static field.
+	   * <p>
+	   * Allocates an application- and implementation-dependent amount of
+	   * memory in the current scope (to represent stack backtrace).
+	   */
     @SCJAllowed
+    @SCJRestricted(maySelfSuspend = false)
     public InaccessibleAreaException() {
     }
 
+    /**
+     * Shall not copy "this" to any instance or
+     * static field. The scope containing the msg argument must enclose the
+     * scope containing "this". Otherwise, an IllegalAssignmentError will be
+     * thrown.
+     * <p>
+     * Allocates an application- and implementation-dependent amount of
+     * memory in the current scope (to represent stack backtrace).
+     */
     @SCJAllowed
+    @SCJRestricted(maySelfSuspend = false)
     public InaccessibleAreaException(String description) {
         super(description);
     }
