dafcf56c1544392b59086530fe2dda767ac3ce81
aplsek <aplsek@gmail.com>
2010-10-07 11:33:03 -0400

    CDj - build file, minor fixes
    Lei: we need to find a standard way how to generate names of the bin files

--- oSCJ/examples/cdj-vanilla-fiji-v1.2/build.sh
+++ oSCJ/examples/cdj-vanilla-fiji-v1.2/build.sh
@@ -9,25 +9,18 @@ FIJI=../../../../
 FIJI_FLAGS="--g-def-max-mem=1300k --g-def-trigger=1m  -G hf --more-opt --max-threads 5 --uniprocessor"
 
 
-#--g-def-max-mem=830k --g-def-trigger=700k
-#--g-def-max-mem=830k --g-def-trigger=500k
-#--g-def-max-mem=1830k --g-def-trigger=1000k
-
 #CLEANUP
 echo "Cleaning class files..."
 find build/ -name "*.class" | xargs rm -f
-<<<<<<< /home/ales/fiji/fiji-filip/scj/oSCJ/examples/cdj-vanilla-fiji-v1.2/build.sh
-=======
 rm -rf cdj.jar
 rm -rf cdj_x86
 rm -rf build/
 mkdir build/
->>>>>>> /tmp/build.sh~other.ipvnRZ
+
 
 
 find . -name *.java > list
 echo "Compiling with javac..."
-#javac src/heap/Main.java -classpath "$FIJI/runtimej/build:$FIJI/lib/fijicore.jar:$FIJI/lib/fivmcommon.jar:$FIJI/lib/fijirt.jar:$FIJI/lib/fivmr.jar" -d ./build
 javac -classpath "$FIJI/runtimej/build:$FIJI/lib/fijicore.jar:$FIJI/lib/fivmcommon.jar:$FIJI/lib/fijirt.jar:$FIJI/lib/fivmr.jar" -d ./build @list
 rm -rf list
 
@@ -38,10 +31,11 @@ cd ..
 
 
 
+
 # COMPILE FIJI 1
 echo "Compiling cdj_x86"
 
-$FIJI/bin/fivmc -o cdj_x86 cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS
+$FIJI/bin/fivmc -o cdj_hf cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS
 
 
 
@@ -60,111 +54,35 @@ FIJI_FLAGS="--g-def-max-mem=1300k --g-def-trigger=1m  -G hf --g-pred-level a --m
 # COMPILE FIJI 1
 echo "Compiling cdj_hg_level_a"
 
-$FIJI/bin/fivmc -o cdj_hg_level_a cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS
+$FIJI/bin/fivmc -o cdj_hf_a cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS 
 
 
-#
-#
-# =====================================================================================
 
 
-FIJI_FLAGS="--g-def-max-mem=1000k --g-def-trigger=100k  -G hf --g-pred-level a --more-opt --max-threads 5 --uniprocessor"
+FIJI_FLAGS="--g-def-max-mem=1300k --g-def-trigger=1m  -G hf --g-pred-level c --more-opt --max-threads 5 --uniprocessor"
 
 
 # COMPILE FIJI 1
-echo "Compiling cdj_300k"
+echo "Compiling cdj_hg_level_a"
 
-$FIJI/bin/fivmc -o cdj_100k cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS
-
-
+$FIJI/bin/fivmc -o cdj_hf_c cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS 
 
 
 
 
 
-
-
-
-#
-#
-# =====================================================================================
-
-
-FIJI_FLAGS="--g-def-max-mem=1000k --g-def-trigger=300k  -G hf --g-pred-level a --more-opt --max-threads 5 --uniprocessor"
+FIJI_FLAGS="--g-def-max-mem=1300k --g-def-trigger=1m  -G hf --g-pred-level cw --more-opt --max-threads 5 --uniprocessor"
 
 
 # COMPILE FIJI 1
-echo "Compiling cdj_100k"
+echo "Compiling cdj_hg_level_a"
 
-$FIJI/bin/fivmc -o cdj_300k cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS
+$FIJI/bin/fivmc -o cdj_hf_cw cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS 
 
-
-
-
-
-#
-#
-# =====================================================================================
-
-
-FIJI_FLAGS="--g-def-max-mem=1000k --g-def-trigger=400k  -G hf --g-pred-level a --more-opt --max-threads 5 --uniprocessor"
-
-
-# COMPILE FIJI 1
-echo "Compiling cdj_100k"
-
-$FIJI/bin/fivmc -o cdj_400k cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS
-
-
-
-
-FIJI_FLAGS="--g-def-max-mem=1000k --g-def-trigger=500k  -G hf --g-pred-level a --more-opt --max-threads 5 --uniprocessor"
-
-
-# COMPILE FIJI 1
-echo "Compiling cdj_500k"
-
-$FIJI/bin/fivmc -o cdj_500k cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS
-
-
-
-
-
-FIJI_FLAGS="--g-def-max-mem=1000k --g-def-trigger=600k  -G hf --g-pred-level a --more-opt --max-threads 5 --uniprocessor"
-
-
-# COMPILE FIJI 1
-echo "Compiling cdj_600k"
-
-$FIJI/bin/fivmc -o cdj_600k cdj.jar --main heap/Main --reflect cdj.reflectlog $FIJI_FLAGS
 
 
 
 
 echo "All done."
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#echo "Compiling cdj_cmr"
-#../fivm_sparc/bin/fivmc -o cdj_cmr cdj.jar --main heap/Main --target=sparc-rtems4.9 --reflect cdj.reflectlog --g-def-max-mem=50M --internal-inst instrumentation.h -G cmr --pollcheck-mode portable --g-def-trigger=30M --more-opt
-
-#echo "Compiling cdj_hf_c"
-#../fivm_sparc/bin/fivmc -o cdj_hf_c cdj.jar --main heap/Main --target=sparc-rtems4.9 --reflect cdj.reflectlog --g-def-max-mem=50M --internal-inst instrumentation.h -G hf --pollcheck-mode portable --g-def-trigger=30M --more-opt --g-pred-level=c
-
-#echo "Compiling cdj_hf_a"
-#../fivm_sparc/bin/fivmc -o cdj_hf_a cdj.jar --main heap/Main --target=sparc-rtems4.9 --reflect cdj.reflectlog --g-def-max-mem=50M --internal-inst instrumentation.h -G hf --pollcheck-mode portable --g-def-trigger=30M --more-opt --g-pred-level=a
-
-#echo "Compiling cdj_hf_cw"
-#../fivm_sparc/bin/fivmc -o cdj_hf_cw cdj.jar --main heap/Main --target=sparc-rtems4.9 --reflect cdj.reflectlog --g-def-max-mem=50M --internal-inst instrumentation.h -G hf --pollcheck-mode portable --g-def-trigger=30M --more-opt --g-pred-level=cw
 
