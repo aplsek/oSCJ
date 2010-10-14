6ee6b597f755675955ee79ae0762d6237b9cdd03
aplsek <aplsek@gmail.com>
2010-10-14 10:26:56 -0400

    MD5 Benchmark - MD5 version for vanilla FijiVM added and modified to collect benchmarking data.

---
+++ oSCJ/examples/fast-md5-vanilla/src/bench/Constants.java
@@ -0,0 +1,25 @@
+package bench;
+
+public class Constants {
+
+	public static int MAX = 10;
+	 
+	 public static int NUMBER_OF_PLANES = 60;
+	 
+	 public static int MISSION_MEMORY =  5000000;
+	 public static int PRIVATE_MEMORY =  5000000;
+	 
+	 public static int RUNS = 1000;
+ 
+	 public static long          PERIOD                          = 50;
+	
+	 public static String[] input = {
+		  "The quick brown fox jumps over the lazy dog",
+		  "The quick brown fox jumps over the lazy dog.",
+		  "Form 3",
+		  "Form 4",
+		  "Form 5"
+		  };
+
+	 
+}
---
+++ oSCJ/examples/fast-md5-vanilla/src/com/twmacinta/util/MyMain.java
@@ -0,0 +1,38 @@
+package com.twmacinta.util;
+
+import bench.Benchmark;
+import bench.Constants;
+
+/**
+ * 
+ * Created by Ales Plsek, October 2010.
+ * @author plsek
+ *
+ */
+
+public class MyMain {
+	
+	
+	 public static void doMD5work() {
+		 for(String in : Constants.input ) {
+			MyMD5Input myMD = new MyMD5Input();
+			myMD.run(in);
+			myMD.finalHash(in);
+		 }
+	 }
+	 
+	 
+	 private static void main(String[] arg) {
+		 Benchmark.init();
+		 
+		 for(int i = 0; i < Constants.RUNS;i++) {
+			 long start = System.nanoTime();
+			 doMD5work();
+			 long  end =   System.nanoTime();
+	         Benchmark.set(start, end);
+		 }
+		 
+		 Benchmark.dumpResults();
+	 }
+	 
+}
