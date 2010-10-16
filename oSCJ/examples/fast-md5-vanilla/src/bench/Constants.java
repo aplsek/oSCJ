c17c4bb12f68ce113852db045803f8d802619368
aplsek <aplsek@gmail.com>
2010-10-16 18:04:31 -0400

    MD5 - vanilla, imput is the same as for SCJ version

--- oSCJ/examples/fast-md5-vanilla/src/bench/Constants.java
+++ oSCJ/examples/fast-md5-vanilla/src/bench/Constants.java
@@ -6,20 +6,42 @@ public class Constants {
 	 
 	 public static int NUMBER_OF_PLANES = 60;
 	 
-	 public static int MISSION_MEMORY =  5000000;
-	 public static int PRIVATE_MEMORY =  5000000;
+	 public static int MISSION_MEMORY =  850000;
+	 public static int PRIVATE_MEMORY =  90000;
 	 
 	 public static int RUNS = 1000;
  
 	 public static long          PERIOD                          = 50;
 	
+	 
 	 public static String[] input = {
 		  "The quick brown fox jumps over the lazy dog",
 		  "The quick brown fox jumps over the lazy dog.",
-		  "Form 3",
-		  "Form 4",
-		  "Form 5"
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",
+		  "The quick brown fox jumps over the lazy dog",
+                  "The quick brown fox jumps over the lazy dog.",		  
 		  };
-
-	 
 }
