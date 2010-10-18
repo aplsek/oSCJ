9f697dfa75b89dbaf93790fe2b13c90874a1e79b
aplsek <aplsek@gmail.com>
2010-10-18 13:44:49 -0400

    Examples : adding buildinf or scj.jar to each build file
    H Enter commit message.  Lines beginning with 'HG:' are removed.

--- oSCJ/examples/microBench/build.sh
+++ oSCJ/examples/microBench/build.sh
@@ -8,6 +8,10 @@ FIJI_HOME="../../../../"
 SCJFLAGS="--scj --scj-scope-backing 10m --g-def-immortal-mem 10m --g-scope-checks no --pollcheck-mode none"   #700 scope, 500 imm
 FIJIFLAGS="--max-threads 5 --more-opt --uniprocessor"  # -v 1
 
+# rebuild SCJ.jar                                                                                 
+SCJ="../../scj/ri"
+CWD=`pwd`
+cd $SCJ && make scj.jar && cd $CWD
 
 # CLEAN-UP
 rm -rf build
