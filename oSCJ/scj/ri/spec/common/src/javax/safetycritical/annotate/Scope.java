bd46b28e98eb625bf8e22fe155a08440d3520c4c
Daniel Tang <dytang@cs.purdue.edu>
2011-03-08 23:50:18 -0500

    split CURRENT into CALLER/THIS. purely a rename, no semantic changes yet

--- oSCJ/scj/ri/spec/common/src/javax/safetycritical/annotate/Scope.java
+++ oSCJ/scj/ri/spec/common/src/javax/safetycritical/annotate/Scope.java
@@ -1,9 +1,10 @@
 package javax.safetycritical.annotate;
 
 public @interface Scope {
-	public static final String UNKNOWN = "UNKNOWN";
-	public static final String CURRENT = "CURRENT";
+	public static final String CALLER = "CURRENT";
 	public static final String IMMORTAL = "IMMORTAL";
+	public static final String THIS = "THIS";
+	public static final String UNKNOWN = "UNKNOWN";
 
 	public String value();
 }
