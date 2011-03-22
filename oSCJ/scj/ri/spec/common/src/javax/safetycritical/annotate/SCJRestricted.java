01cfb80b509417e611292c314fd342cdba9e603f
aplsek <aplsek@gmail.com>
2011-03-22 14:35:48 -0400

    @SCJRestricted - small update

--- oSCJ/scj/ri/spec/common/src/javax/safetycritical/annotate/SCJRestricted.java
+++ oSCJ/scj/ri/spec/common/src/javax/safetycritical/annotate/SCJRestricted.java
@@ -8,7 +8,12 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.CLASS)
 @Target({ ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR })
 public @interface SCJRestricted {
+    /** The phase of the mission in which a method may run. */
     public Phase[] value() default { Phase.ALL };
+    
+    /** Marks whether or not a method may allocate memory. */
     public boolean mayAllocate()    default true;
+    
+    /** Marks whether or not a method may execute a blocking operation. */
     public boolean maySelfSuspend() default false;
 }
