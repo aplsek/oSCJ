31b9942e93cbc499e67261ba44d2bac8e7d76841
aplsek <aplsek@gmail.com>
2011-04-18 04:37:51 -0400

    webserver example - adding the missing microedition classes, it should compile now.

---
+++ oSCJ/examples/webserver/src/javax/microedition/io/StreamConnectionNotifier.java
@@ -0,0 +1,10 @@
+package javax.microedition.io;
+
+public class StreamConnectionNotifier {
+
+    public StreamConnection acceptAndOpen() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
